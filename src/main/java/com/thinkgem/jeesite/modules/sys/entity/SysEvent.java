00596a5c686107e50c33dcc025b738246988c03b
Jianqi <a1752399@student.adelaide.edu.au>
2019-10-29 00:30:05 +1030

    I don't wanna do || Add some files

---
+++ src/main/java/com/thinkgem/jeesite/modules/sys/entity/SysEvent.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright &copy; 2012-2016 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.thinkgem.jeesite.modules.sys.entity;
+
+import org.hibernate.validator.constraints.Length;
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import javax.validation.constraints.NotNull;
+
+import com.thinkgem.jeesite.common.persistence.DataEntity;
+
+/**
+ * eventEntity
+ * @author ZJQ
+ * @version 2019-10-29
+ */
+public class SysEvent extends DataEntity<SysEvent> {
+	
+	private static final long serialVersionUID = 1L;
+	private String eventid;		// event ID
+	private String eventname;		// Event Name
+	private String eventdescription;		// Event Description
+	private Date dueDate;		// due time
+	
+	public SysEvent() {
+		super();
+	}
+
+	public SysEvent(String id){
+		super(id);
+	}
+
+	@Length(min=1, max=64, message="event ID长度必须介于 1 和 64 之间")
+	public String getEventid() {
+		return eventid;
+	}
+
+	public void setEventid(String eventid) {
+		this.eventid = eventid;
+	}
+	
+	@Length(min=1, max=64, message="Event Name长度必须介于 1 和 64 之间")
+	public String getEventname() {
+		return eventname;
+	}
+
+	public void setEventname(String eventname) {
+		this.eventname = eventname;
+	}
+	
+	@Length(min=1, max=200, message="Event Description长度必须介于 1 和 200 之间")
+	public String getEventdescription() {
+		return eventdescription;
+	}
+
+	public void setEventdescription(String eventdescription) {
+		this.eventdescription = eventdescription;
+	}
+	
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@NotNull(message="due time不能为空")
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+	
+}
---
+++ src/main/java/com/thinkgem/jeesite/modules/sys/service/SysEventService.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright &copy; 2012-2016 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.thinkgem.jeesite.modules.sys.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.thinkgem.jeesite.common.persistence.Page;
+import com.thinkgem.jeesite.common.service.CrudService;
+import com.thinkgem.jeesite.modules.sys.entity.SysEvent;
+import com.thinkgem.jeesite.modules.sys.dao.SysEventDao;
+
+/**
+ * eventService
+ * @author ZJQ
+ * @version 2019-10-29
+ */
+@Service
+@Transactional(readOnly = true)
+public class SysEventService extends CrudService<SysEventDao, SysEvent> {
+
+	public SysEvent get(String id) {
+		return super.get(id);
+	}
+	
+	public List<SysEvent> findList(SysEvent sysEvent) {
+		return super.findList(sysEvent);
+	}
+	
+	public Page<SysEvent> findPage(Page<SysEvent> page, SysEvent sysEvent) {
+		return super.findPage(page, sysEvent);
+	}
+	
+	@Transactional(readOnly = false)
+	public void save(SysEvent sysEvent) {
+		super.save(sysEvent);
+	}
+	
+	@Transactional(readOnly = false)
+	public void delete(SysEvent sysEvent) {
+		super.delete(sysEvent);
+	}
+	
+}
